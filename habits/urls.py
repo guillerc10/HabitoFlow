3ba353f21f802a67d846f7287b8eab24057a3ff1
guillerc10 <guillermocaceres164@gmail.com>
2026-08-18 01:44:15 -0400

    FUNCIONANDO

--- habits/urls.py
+++ habits/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.habit_list, name='habit_list'),
+    path('habito/<int:habit_id>/', views.habit_detail, name='habit_detail'),
 ]
--- habits/views.py
+++ habits/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
-from .models import Habit
+from django.shortcuts import render, get_object_or_404
+from .models import Habit, HabitLog
 
 def habit_list(request):
     habits = Habit.objects.all()
     return render(request, 'habits/habit_list.html', {'habits': habits})
+
+
+def habit_detail(request, habit_id):
+    habit = get_object_or_404(Habit, id=habit_id)
+    logs = HabitLog.objects.filter(habit=habit).order_by('-date')
+    return render(request, 'habits/habit_detail.html', {'habit': habit, 'logs': logs})

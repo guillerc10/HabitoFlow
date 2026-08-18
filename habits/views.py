65a0125d76288354942129643ee42d937e3c01a6
guillerc10 <guillermocaceres164@gmail.com>
2026-08-18 01:16:40 -0400

    PRIMERAS PRUEBAS FUNCIONALES

--- habits/views.py
+++ habits/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Habit
 
-# Create your views here.
+def habit_list(request):
+    habits = Habit.objects.all()
+    return render(request, 'habits/habit_list.html', {'habits': habits})
